b544e614acc63c6f454f43e0239521222260ce27
Tomas Filip <tomas@ecoding.sk>
2023-10-12 18:03:26 +0200

    Cleared up the code and removed obsolete stuff

--- Dto/Shared/NumberOfCallsPerDayDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ECoding.SimpleApi.Core.SDK.Dto
-{
-    public class NumberOfCallsPerDayDto
-    {
-        public DateTime Day { get; set; }
-        public int NumberOfCallsPerDay { get; set; }
-    }
-}
